efe26c9e819a4d34a521d0ec8ef0a8cb7673149b
Rohan <rohangupta112005@gmail.com>
2026-06-11 15:45:31 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for(char c : s){
+            if(isalnum(c)) temp += c;
+        }
+
+        cout << temp;
+        int i = 0;
+        int j = temp.length() - 1;
+
+        while(i <= j){
+            if(tolower(temp[i++]) != tolower(temp[j--])) return false;
+        }
+        return true;
+    }
+};
