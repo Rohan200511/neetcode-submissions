bf15c925234b5473893f9536c8f39950413feaa7
Rohan <rohangupta112005@gmail.com>
2026-07-09 14:43:17 +0530

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_map<string , priority_queue<string, vector<string>, greater<string>>>adj;
+    vector<string>ans;
+
+    void dfs(string src){
+
+        while(!adj[src].empty()){
+            string next = adj[src].top();
+            adj[src].pop();
+            dfs(next);
+        }
+
+        ans.push_back(src);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+
+        for(auto& t : tickets){
+            string u = t[0];
+            string v = t[1];
+
+            adj[u].push(v);
+        }
+
+        dfs("JFK");
+
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
